71d9faa74aff8aac8b42c0d9c6e3423913ad5b94
ivy <yookyung.lee@carrotins.com>
2022-11-18 17:21:12 +0900

    자바 강의 2탄을 위하여 정리

--- 1.완전 기초 프로그래밍 강좌/11_arrarylist/array_list.java
+++ 1.완전 기초 프로그래밍 강좌/11_arrarylist/array_list.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 
-class ArraryLists {
+class ArraryList {
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<Integer>();
 
